ce60775dcc7f40ff2eee41a49841c904fe32a6ce
kwin11 <kevinpatel1142@gmail.com>
2023-10-25 16:33:36 -0400

    Fix the api for compiler

--- code editor UI/vertex-node-backend/compiler-server.js
+++ code editor UI/vertex-node-backend/compiler-server.js
@@ -1,47 +1,58 @@
-const express = require('express');
-const cors = require('cors');
-const Axios = require('axios');
+const express = require("express");
+const cors = require("cors");
+const Axios = require("axios");
 const app = express();
 const PORT = 9000;
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/compile', (req, res) => {
-    // Receive the required data from the request
-    let code = req.body.code;
-    let langauge = req.body.language;
-    let input = req.body.input;
+app.post("/compile", (req, res) => {
+  // Receive the required data from the request
+ // let code = req.body.code;
+  let language = req.body.language;
+  let input = req.body.input;
 
-    if (langauge == 'python') {
-        language = 'py'
-    }
+  if (language == "python") {
+    language = "py";
+  }
 
-    let data = ({
-        "code": code,
-        "langauge": language,
-        "input": input
+//   let data = {
+//     code: code,
+//     langauge: language,
+//     input: input,
+//   };
+  const encodedParams = new URLSearchParams();
+  encodedParams.append(
+    "code",
+    req.body.code
+  );
+
+  encodedParams.append("language", "java");
+  console.log(encodedParams);
+
+  let config = {
+    method: "post",
+    url: "https://codex7.p.rapidapi.com/",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "X-RapidAPI-Host": "codex7.p.rapidapi.com",
+      "X-RapidAPI-Key": "fcc2d6c6b3msh404adbe533c36f5p148528jsn4a93557ee62f",
+    },
+    data: encodedParams,
+  };
+
+  // Call the code compilation API
+  Axios(config)
+    .then((response) => {
+      res.send(response.data);
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
     });
-
-    let config = {
-        method: "post",
-        url: 'https://codexweb.netlify.app/.netlify/functions/enforceCode',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        data: data
-    };
-
-    // Call the code compilation API
-    Axios(config)
-        .then((response) => {
-            res.send(response.data)
-            console.log(response.data)
-        }).catch((error) => {
-            console.log(error);
-        });
-})
+});
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
--- code editor UI/vertex-node-backend/server.js
+++ code editor UI/vertex-node-backend/server.js
@@ -6,8 +6,14 @@ const PORT = 8000;
 
 app.use(bodyParser.json())
 
+app.get('/api/kevin', (req, res) => {
+   //const { query } = req.body;
+    res.json({ response: "kevin" });
+}); 
 app.post('/api/query-vertex', (req, res) => {
     const { query } = req.body;
+
+    
     res.json({ response: query });
 });
 
